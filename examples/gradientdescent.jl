c84a75babb1c546522a65c0ea99354cf14b69824
Ryan Senne <50930199+rsenne@users.noreply.github.com>
2026-01-01 18:06:04 -0500

    Format and fix typos.

--- examples/gradientdescent.jl
+++ examples/gradientdescent.jl
@@ -1,34 +1,53 @@
 # # Gradient Descent in HMMs
 
-#= In this tutorial we will explore two ways one can use gradient descent when fitting HMMs. There are two ways one typically wants to use gradient descent in HMMs:
-1. Fitting parameters of the observation model that do not have closed form updates, e.g., GLMs, neural networks, etc.
-2. Fitting the entire HMM using gradient based optimization by usingg automatic differentiation.
-We will explore both of these in this tutorial.
-=# 
+#= 
+In this tutorial we explore two ways to use gradient descent when fitting HMMs:
+
+1. Fitting parameters of an observation model that do not have closed-form updates
+   (e.g., GLMs, neural networks, etc.).
+2. Fitting the entire HMM with gradient-based optimization by leveraging automatic
+   differentiation.
+
+We will explore both approaches below.
+=#
+
 using DensityInterface
 using Distributions
 using HiddenMarkovModels
-# using HMMTest  #src
 using LinearAlgebra
 using Optim
 using Random
 using StableRNGs
 using StatsAPI
-using Test  #src
-rng = StableRNG(42);
+using Test
 
-#= For both parts of this tutorial we will use a simple HMM with Gaussian observations. Of course, using gradient descent in either of these examples in completely overkill, but it serves to illustrate the methods. 
-Let's begin by creating a Normal distirebution model.=#
+rng = StableRNG(42)
+
+#= 
+For both parts of this tutorial we use a simple HMM with Gaussian observations.
+Using gradient-based optimization here is overkill, but it keeps the tutorial
+simple while illustrating the relevant methods.
+
+We begin by defining a Normal observation model.
+=#
 
 mutable struct NormalModel{T}
     μ::T
-    logσ::T # unconstrained parameterization; σ = exp(logσ)
+    logσ::T  # unconstrained parameterization; σ = exp(logσ)
 end
 
 σ(mod::NormalModel) = exp(mod.logσ)
 
-#= We have defined a simple probability model with two parameters: the mean and the log of the standard deviation. Using the log is intentional so that we can optimize over all real numbers without worrying about the positivity constraint on the standard deviation. But, we must now provide the common interface HiddenMarkovModels.jl requires (e.g., logdensityof, rand, fit!)=#
-function DensityInterface.logdensityof(mod::NormalModel, obs::T) where T<:Real
+#= 
+We have defined a simple probability model with two parameters: the mean and the
+log of the standard deviation. Using logσ is intentional so we can optimize over
+all real numbers without worrying about the positivity constraint on σ.
+
+Next, we provide the minimal interface expected by HiddenMarkovModels.jl:
+(logdensityof, rand, fit!).
+=#
+
+function DensityInterface.logdensityof(mod::NormalModel, obs::T) where {T<:Real}
     s = σ(mod)
     return -0.5 * log(2π) - log(s) - 0.5 * ((obs - mod.μ) / s)^2
 end
@@ -39,23 +58,32 @@ function Random.rand(rng::AbstractRNG, mod::NormalModel)
     return rand(rng, Normal(mod.μ, σ(mod)))
 end
 
-#= Because we are fitting a Gaussian, where the variance can collapse to zero, we will add some weak priors on the parameters to prevent this from happening. We will use a weak Normal prior on the mean and a prior on the log standard deviation that pulls it toward 1 with moderate strength. =#
-const μ_prior     = Normal(0.0, 10.0)          # weak prior on mean
-const logσ_prior  = Normal(log(1.0), 0.5)      # prior pulling σ toward ~1 with moderate strength
+#= 
+Because we are fitting a Gaussian (and the variance can collapse to ~0), we add
+weak priors to regularize the parameters. We use:
+- A weak Normal prior on μ
+- A moderate-strength Normal prior on logσ that pulls σ toward ~1
+=#
 
-function StatsAPI.fit!(mod::NormalModel, data::AbstractVector{<:Real}, weights::AbstractVector{<:Real})
+const μ_prior    = Normal(0.0, 10.0)
+const logσ_prior = Normal(log(1.0), 0.5)
 
+function StatsAPI.fit!(
+    mod::NormalModel,
+    data::AbstractVector{<:Real},
+    weights::AbstractVector{<:Real},
+)
     function neglogpost(θ)
         μ, logσ = θ
 
-        # likelihood term (weighted)
+        # Weighted negative log-likelihood
         nll = 0.0
         tmp = NormalModel(μ, logσ)
         for (y, w) in zip(data, weights)
             nll += -w * logdensityof(tmp, y)
         end
 
-        # prior terms (MAP = maximize loglik + logprior  <=> minimize -loglik - logprior)
+        # Negative log-prior terms (MAP)
         nll += -logpdf(μ_prior, μ)
         nll += -logpdf(logσ_prior, logσ)
 
@@ -68,74 +96,110 @@ function StatsAPI.fit!(mod::NormalModel, data::AbstractVector{<:Real}, weights::
     return mod
 end
 
-#= Now that we have fully defined our observation model, we can create an HMM using it.=#
-init_dist = [0.2, 0.7, 0.1]
-init_trans = [0.9 0.05 0.05; 0.075 0.9 0.025; 0.1 0.1 0.8]
-obs_dists = [NormalModel(-3.0, log(0.25)), NormalModel(0.0, log(0.5)), NormalModel(3.0, log(0.75))]
+#= 
+Now that we have fully defined our observation model, we can create an HMM using it.
+=#
 
-hmm_true = HMM(init_dist, init_trans, obs_dists);
+init_dist  = [0.2, 0.7, 0.1]
+init_trans = [0.9 0.05 0.05;
+              0.075 0.9 0.025;
+              0.1 0.1 0.8]
 
-#= We can now generate some data from this HMM.=#
-state_seq, obs_seq = rand(rng, hmm_true, 10000);
+obs_dists = [
+    NormalModel(-3.0, log(0.25)),
+    NormalModel( 0.0, log(0.5)),
+    NormalModel( 3.0, log(0.75)),
+]
 
-#= Now we can fit a new HMM to this data using gradient descent to fit the observation model parameters. We will create a set of guesses for the initial parameters then fit the new model.=#
-init_dist_guess = fill(1.0 / 3, 3)
-init_trans_guess = [0.98 0.01 0.01; 0.01 0.98 0.01; 0.01 0.01 0.98]
-obs_dist_guess = [NormalModel(-2.0, log(1.0)), NormalModel(2.0, log(1.0)), NormalModel(0.0, log(1.0))]
+hmm_true = HMM(init_dist, init_trans, obs_dists)
+
+#= 
+We can now generate data from this HMM.
+Note: rand(rng, hmm, T) returns (state_seq, obs_seq).
+=#
+
+state_seq, obs_seq = rand(rng, hmm_true, 10_000)
+
+#= 
+Next we fit a new HMM to this data. Baum–Welch will perform EM updates for the
+HMM parameters; during the M-step, our observation model parameters are fit via
+gradient-based optimization (BFGS).
+=#
+
+init_dist_guess  = fill(1.0 / 3, 3)
+init_trans_guess = [0.98 0.01 0.01;
+                    0.01 0.98 0.01;
+                    0.01 0.01 0.98]
+
+obs_dist_guess = [
+    NormalModel(-2.0, log(1.0)),
+    NormalModel( 2.0, log(1.0)),
+    NormalModel( 0.0, log(1.0)),
+]
 
 hmm_guess = HMM(init_dist_guess, init_trans_guess, obs_dist_guess)
 
 hmm_est, lls = baum_welch(hmm_guess, obs_seq)
 
 #= 
-Great! We were able to fit the model using gradient descent within EM. Now let's explore how we can fit the entire HMM using gradient descent by leveraging automatic differentiation.
-To do this, we will leverage the fact that forward algorithm of an HMM implicitly marginalizes the latent states, allowing us to compute the likelihood of the observations directly given the model parameters.
-Thus, we can use the returned loglikelihood of the forward algorithm as our objective function to minimize (negative loglikelihood) with respect to all model parameters. It is worthwhile too think about this though,
-each time we evaluate the objective function we must run the forward algorithm, which can be computationally expensive for large datasets. But, this method allows us to fit any parameterized HMM using gradient descent. But, we will get approximate
-second-order information via BFGS to help speed up convergence.
+Great! We were able to fit the model using gradient descent inside EM.
+
+Now we will fit the entire HMM using gradient-based optimization by leveraging
+automatic differentiation. The key idea is that the forward algorithm marginalizes
+out the latent states, providing the likelihood of the observations directly as a
+function of all model parameters.
+
+We can therefore optimize the negative log-likelihood returned by `forward`.
+Each objective evaluation runs the forward algorithm, which can be expensive for
+large datasets, but this approach allows end-to-end gradient-based fitting for
+arbitrary parameterized HMMs.
+
+To respect HMM constraints, we optimize unconstrained parameters and map them to
+valid probability distributions via softmax:
+- π = softmax(ηπ)
+- each row of A = softmax(row logits)
 =#
 
-# stable softmax
+# Stable softmax
 function softmax(v::AbstractVector)
     m = maximum(v)
     ex = exp.(v .- m)
-    ex ./ sum(ex)
+    return ex ./ sum(ex)
 end
 
 function rowsoftmax(M::AbstractMatrix)
-    K = size(M, 1)
     A = similar(M)
-    for i in 1:K
+    for i in 1:size(M, 1)
         A[i, :] .= softmax(view(M, i, :))
     end
     return A
 end
 
-#= To use gradient descent on an HMM, we need to be able to respect the constraints of the model. Specifically, the initial state distribution and transition matrix must be valid probability distributions (i.e., non-negative and sum to one). 
-We achieve this by parameterizing these components in an unconstrained space and then applying the softmax function to map them back to the probability simplex. This is achieved via the softmax function. We could have also used the approriate Lagrange multipliers.=# 
 function unpack_to_hmm(θ::AbstractVector, K::Int)
     idx = 1
 
-    ηπ = @view θ[idx:idx+K-1]; idx += K
-    ηA = reshape(@view(θ[idx:idx+K*K-1]), K, K); idx += K*K
-    μ  = @view θ[idx:idx+K-1]; idx += K
+    ηπ   = @view θ[idx:idx+K-1]; idx += K
+    ηA   = reshape(@view(θ[idx:idx+K*K-1]), K, K); idx += K*K
+    μ    = @view θ[idx:idx+K-1]; idx += K
     logσ = @view θ[idx:idx+K-1]; idx += K
 
     π = softmax(ηπ)
     A = rowsoftmax(ηA)
-    obs = [NormalModel(μ[k], logσ[k]) for k in 1:K]
-    return HMM(π, A, obs)
+    dists = [NormalModel(μ[k], logσ[k]) for k in 1:K]
+
+    return HMM(π, A, dists)
 end
 
-# Convert a valid HMM into an unconstrained θ
+# Convert a valid HMM into an unconstrained parameter vector θ
 function hmm_to_θ0(hmm::HMM)
     K = length(hmm.init)
 
     π = hmm.init
     A = hmm.trans
 
-    ηπ = log.(π .+ eps())               # avoid -Inf
-    ηA = log.(A .+ eps())               # same
+    # Logits; any additive constant is irrelevant under softmax
+    ηπ = log.(π .+ eps())
+    ηA = log.(A .+ eps())
 
     μ    = [hmm.dists[k].μ    for k in 1:K]
     logσ = [hmm.dists[k].logσ for k in 1:K]
@@ -155,12 +219,16 @@ K = 3
 obj(θ) = negloglik_from_θ(θ, obs_seq, K)
 
 result = Optim.optimize(obj, θ0, BFGS(); autodiff = :forward)
+hmm_est2 = unpack_to_hmm(result.minimizer, K)
 
-hmm_est2 = unpack_to_hmm(result.minimizer, K);
+#= 
+We have now trained an HMM using gradient-based optimization over *all* parameters!
+Let’s quickly verify that it matches the EM solution above (up to numerical tolerance).
+=#
 
-#= We have now trained an HMM using gradient descent on all of the parameters! Let's quickly quick it matches the results we ggot fropm EM above.=#
 isapprox(hmm_est.init, hmm_est2.init; atol=1e-3)
 isapprox(hmm_est.trans, hmm_est2.trans; atol=1e-3)
+
 for k in 1:K
     isapprox(hmm_est.dists[k].μ, hmm_est2.dists[k].μ; atol=1e-3)
     isapprox(σ(hmm_est.dists[k]), σ(hmm_est2.dists[k]); atol=1e-3)
